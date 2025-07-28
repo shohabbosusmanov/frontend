915dc0dbe56e4f4ecd38a9465e770489f2915be4
Shohabbos Usmanov <shohabbosusmanov@gmail.com>
2025-07-28 13:51:39 +0500

    s

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,7 +4,4 @@ export const axiosInstance = axios.create({
     baseURL: "https://backend-production-2aa5.up.railway.app/api",
     withCredentials: true,
     timeout: 5000,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-    },
 });
